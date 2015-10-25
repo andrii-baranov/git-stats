2bb04a529506d67129912879be35fb4278706fd3
AB <andrii.baranov@gmail.com>
2015-10-25 13:09:36 +0200

    Initial commit

---
+++ GitStats.Core/Actors/StatsCollectorActor.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using GitStats.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStats.Core.Actors
+{
+    public class StatsCollectorActor : ReceiveActor
+    {
+        protected ILoggingAdapter Log = Context.GetLogger();
+
+        public StatsCollectorActor()
+        {
+            Receive<StatsRequest>(message => 
+            {
+                Log.Info("Message received {0}", message.RepoName);
+            });
+        }
+    }
+}
