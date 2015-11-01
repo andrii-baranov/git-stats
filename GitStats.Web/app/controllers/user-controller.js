f7c2ff1c3cbfd33261f39acc8a05c1e5cfaa24b3
AB <andrii.baranov@gmail.com>
2015-11-02 01:06:57 +0200

    Added navigation to details page

---
+++ GitStats.Web/app/controllers/user-controller.js
@@ -0,0 +1,14 @@
+﻿//here we need to inject ui.router as we using it for routing
+var myDemoApp = angular.module('myDemoApp');
+
+myDemoApp.controller('UserCtrl', ['$scope', '$http', '$interval', 'RepoService', 'uiGridConstants', function ($scope, $http, $interval, RepoService, uiGridConstants) {
+
+    $scope.data = [
+        { name : 'a', last : 'b', age : 45},
+        { name: 'a', last: 'b', age: 45 },
+        { name: 'a', last: 'b', age: 45 },
+        { name: 'a', last: 'b', age: 45 },
+        { name: 'a', last: 'b', age: 45 },
+        { name: 'a', last: 'b', age: 45 },
+    ];
+}]);
