2573867db348bb6a406e5a7a239521b7e63e90b7
Andrii Baranov <andsoulnd@gmail.com>
2015-12-26 19:11:09 +0200

    added mongodb repository

---
+++ GitStats.Data/MongoDbRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using GitStats.Domain.Models;
+using MongoDB.Bson;
+
+namespace GitStats.Data
+{
+    public class MongoDbRepository
+    {
+        IMongoDatabase _database;
+        MongoClient _client;
+        public MongoDbRepository(string connectionString)
+        {
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase("test");
+        }
+
+        public List<Repository> Get()
+        {
+            //List<Repository> items = new List<Repository>();
+
+            var collection = _database.GetCollection<Repository>("repo");
+
+            var filter = new BsonDocument();
+            var result = collection.Find(filter).ToList();
+
+            return result;
+        }
+
+        public void AddRepo(Repository repo)
+        {
+            var collection = _database.GetCollection<Repository>("repo");
+            collection.InsertOne(repo);
+        }
+
+    }
+}
--- GitStats/Program.cs
+++ GitStats/Program.cs
@@ -2,6 +2,8 @@
 using GitStats.Core;
 using GitStats.Core.Actors;
 using GitStats.Core.Messages;
+using GitStats.Data;
+using GitStats.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,28 +16,49 @@ namespace GitStats
     {
         static void Main(string[] args)
         {
-            var system = ActorSystem.Create("GitStats");
-            var repoActor = system.ActorOf(Props.Create(() => new StatsCollectorActor()), "repoHandler");
-
-            bool exit = false;
-
-            while (!exit)
+            try
             {
-                Console.WriteLine("Enter repo name:");
+                var repo = new MongoDbRepository("mongodb://localhost:27017/test");
 
-                string repoName = Console.ReadLine();
+                //Repository testData = new Repository
+                //{
+                //    Author = "AB",
+                //    Description = "Desc",
+                //    Name = "Repo Name",
+                //    Url = "https://github.com/andrii-baranov/git-stats"
+                //};
 
-                if (repoName != "exit")
-                {
-                    repoActor.Tell(new StatsRequest(repoName));
-                } 
-                else
-                {
-                    exit = true;
-                }
+                //repo.AddRepo(testData);
+
+                var items = repo.Get();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
 
-            system.Shutdown();
+            //var system = ActorSystem.Create("GitStats");
+            //var repoActor = system.ActorOf(Props.Create(() => new StatsCollectorActor()), "repoHandler");
+
+            //bool exit = false;
+
+            //while (!exit)
+            //{
+            //    Console.WriteLine("Enter repo name:");
+
+            //    string repoName = Console.ReadLine();
+
+            //    if (repoName != "exit")
+            //    {
+            //        repoActor.Tell(new StatsRequest(repoName));
+            //    } 
+            //    else
+            //    {
+            //        exit = true;
+            //    }
+            //}
+
+            //system.Shutdown();
 
             Console.ReadKey();
         }
